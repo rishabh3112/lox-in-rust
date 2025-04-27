903e86e0233c223ed5099fd1dc5dad0b58997489
Rishabh Chawla <rishabh31121999@gmail.com>
2025-04-27 06:05:47 +0000

    chore: parse unary expression

--- src/token.rs
+++ src/token.rs
@@ -146,7 +146,6 @@ impl TokenType {
     }
 
     pub fn value(&self) -> String {
-        println!("token {}", self.name());
         match self {
             TokenType::STRING(str) => str.clone(),
             TokenType::NUMBER(num) => format!("{:?}", num),
